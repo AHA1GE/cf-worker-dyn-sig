40dc993be3e7fa6f3a6190b6d78635503c51051a
Harry Sun <syfsyf@me.com>
2024-03-06 17:05:03 +0800

    updte index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// TODO: factor out functions, separately treat China IP to fix the geolocation shifting issue 
+// TODO: separately treat China IP to fix the geolocation shifting issue 
 import { UAParser } from 'ua-parser-js';
 import { config } from './config';
 import { wmoCodes } from './wmoWeatherCodes';
@@ -62,6 +62,119 @@ async function handleRequest(request: Request, env: Env): Promise<Response> {
 
 async function dynamicSignature(request: Request, env: Env, footNote?: string) {
 
+	const token = env.WEATHER_TOKEN;
+	const ip = request.headers.get("cf-connecting-ip") || ''; //user's  ip
+	const finalData = await getUserData(ip, token, request);
+
+	/****************************************************************
+	const location = { lat: ipData.lat, lon: ipData.lon }
+	//use location to determine weather
+	const weatherApiAddress = `https://api.open-meteo.com/v1/forecast?`;
+	const weatherApiParams = `&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`
+	const weatherResponse = await fetch(weatherApiAddress + `latitude=${location.lat}&longitude=${location.lon}` + weatherApiParams);
+	const weatherData: any = await weatherResponse.json();
+	const weather = weatherData.current.weather_code;
+	const temperature = weatherData.current.temperature_2m;
+
+	// today in history
+	const todayResponse = await fetch('https://xhboke.com/mz/today.php');
+	const todayData = await todayResponse.json();
+	const today = todayData.cover.title;
+
+	 * REWRITED USING CLOUDLFARE WORKER IMAGE API
+	let img = await Jimp.read('./xhxh.jpg');
+	let font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+	img.print(font, 10, 10, 'Welcome, friend from ' + country + '-' + region);
+	img.print(font, 10, 50, 'Today is ' + new Date().toLocaleDateString() + ', ' + ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]);
+	img.print(font, 10, 90, 'Your IP is: ' + ip + ' ' + weather + ' ' + temperature + '℃');
+	img.print(font, 10, 130, 'You are using ' + os);
+	img.print(font, 10, 170, 'Your browser is ' + browser);
+	img.print(font, 10, 210, footNote ? footNote : 'Powered by Cloudflare Workers');
+	const buffer = await img.getBufferAsync(Jimp.MIME_JPEG);
+	return new Response(buffer, {
+		headers: {
+			'Content-Type': 'image/jpeg'
+		}
+	});
+	****************************************************************/
+
+	const text = {
+		line1: `Hello! friend from ${finalData.ipText}.`,
+		line2: `Today is ${new Date().toLocaleDateString()}, ${['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]}`,
+		line3: `It is ${finalData.temperature}°C and ${finalData.weather} at:`,
+		line4: `${finalData.geoCity}.`,
+		line5: `You are using ${finalData.browser} on ${finalData.os}.`,
+		line6: footNote ? footNote : 'Powered by Cloudflare Workers.'
+	}
+	console.log(text);
+
+	// use cloudflare worker image api
+	const tti = 'https://text-to-image.examples.workers.dev/?' //text To Image Api Address
+	const imageURL = 'https://github.com/AHA1GE/IP/blob/master/xhxh.jpg?raw=true';
+	const draw: RequestInitCfPropertiesImageDraw[] = [ //draw text line by line, encode the text to uri first
+		{
+			url: tti + encodeURIComponent(text.line1),
+			height: 20,
+			fit: 'scale-down',
+			left: 15,
+			top: 20,
+		},
+		{
+			url: tti + encodeURIComponent(text.line2),
+			height: 20,
+			fit: 'scale-down',
+			left: 15,
+			top: 55,
+		},
+		{
+			url: tti + encodeURIComponent(text.line3),
+			height: 20,
+			fit: 'scale-down',
+			left: 15,
+			top: 90,
+		},
+		{
+			url: tti + encodeURIComponent(text.line4),
+			height: 20,
+			fit: 'scale-down',
+			left: 15,
+			top: 125,
+		},
+		{
+			url: tti + encodeURIComponent(text.line5),
+			height: 20,
+			fit: 'scale-down',
+			left: 15,
+			top: 160,
+		},
+		{
+			url: tti + encodeURIComponent(text.line6),
+			height: 20,
+			fit: 'scale-down',
+			left: 15,
+			top: 185,
+		},
+	];
+
+	const finalResponse = await fetch(imageURL, {
+		cf: {
+			image: {
+				width: 500,
+				height: 210,
+				draw: draw,
+			},
+		},
+	});
+
+	if (finalResponse.ok || finalResponse.redirected) {
+		return finalResponse
+	} else {
+		// Change to a URL on your server
+		return fetch("https://filesamples.com/samples/image/jpg/sample_640%C3%97426.jpg")
+	}
+}
+
+async function getUserData(ip: string, token: string, request: Request): Promise<any> {
 	// ua
 	const ua: string = request.headers.get("user-agent") || '';
 	const parser = new UAParser(ua);
@@ -75,11 +188,8 @@ async function dynamicSignature(request: Request, env: Env, footNote?: string) {
 	// use cloudlfare provided location
 	const latitude = request.cf?.latitude;
 	const longitude = request.cf?.longitude;
-	// use ip
-	const ip = request.headers.get("cf-connecting-ip") || '';
 
 	// get temprature data
-	const token = env.WEATHER_TOKEN;
 	const tempratureEndpoint = "https://api.waqi.info/feed";
 	let tempratureApi = `${tempratureEndpoint}/geo:${latitude};${longitude}/?token=${token}`;
 	const tempratureInit = { headers: { "content-type": "application/json;charset=UTF-8", }, };
@@ -126,122 +236,5 @@ async function dynamicSignature(request: Request, env: Env, footNote?: string) {
 		os: uaData.os,
 		browser: uaData.browser
 	}
-
-	/****************************************************************
-	const location = { lat: ipData.lat, lon: ipData.lon }
-	//use location to determine weather
-	const weatherApiAddress = `https://api.open-meteo.com/v1/forecast?`;
-	const weatherApiParams = `&current=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto`
-	const weatherResponse = await fetch(weatherApiAddress + `latitude=${location.lat}&longitude=${location.lon}` + weatherApiParams);
-	const weatherData: any = await weatherResponse.json();
-	const weather = weatherData.current.weather_code;
-	const temperature = weatherData.current.temperature_2m;
-
-	// today in history
-	const todayResponse = await fetch('https://xhboke.com/mz/today.php');
-	const todayData = await todayResponse.json();
-	const today = todayData.cover.title;
-	****************************************************************/
-
-
-	/****************************************************************
-	 * REWRITED USING CLOUDLFARE WORKER IMAGE API
-	 * 
-	let img = await Jimp.read('./xhxh.jpg');
-	let font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-	img.print(font, 10, 10, 'Welcome, friend from ' + country + '-' + region);
-	img.print(font, 10, 50, 'Today is ' + new Date().toLocaleDateString() + ', ' + ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]);
-	img.print(font, 10, 90, 'Your IP is: ' + ip + ' ' + weather + ' ' + temperature + '℃');
-	img.print(font, 10, 130, 'You are using ' + os);
-	img.print(font, 10, 170, 'Your browser is ' + browser);
-	img.print(font, 10, 210, footNote ? footNote : 'Powered by Cloudflare Workers');
-	const buffer = await img.getBufferAsync(Jimp.MIME_JPEG);
-	return new Response(buffer, {
-		headers: {
-			'Content-Type': 'image/jpeg'
-		}
-	});
-	****************************************************************/
-
-
-	const text = {
-
-		line1: `Hello! friend from ${finalData.ipText}.`,
-
-		line2: `Today is ${new Date().toLocaleDateString()}, ${['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][new Date().getDay()]}`,
-
-		line3: `It is ${finalData.temperature}°C and ${finalData.weather} at:`,
-
-		line4: `${finalData.geoCity}.`,
-
-		line5: `You are using ${finalData.browser} on ${finalData.os}.`,
-
-		line6: footNote ? footNote : 'Powered by Cloudflare Workers.'
-	}
-	console.log(text);
-
-	// use cloudflare worker image api
-	const tti = 'https://text-to-image.examples.workers.dev/?' //text To Image Api Address
-	const imageURL = 'https://github.com/AHA1GE/IP/blob/master/xhxh.jpg?raw=true';
-	const draw: RequestInitCfPropertiesImageDraw[] = [ //draw text line by line, encode the text to uri first
-		{
-			url: tti + encodeURIComponent(text.line1),
-			height: 27,
-			fit: 'contain',
-			left: 15,
-			top: 20,
-		},
-		{
-			url: tti + encodeURIComponent(text.line2),
-			height: 27,
-			fit: 'contain',
-			left: 15,
-			top: 55,
-		},
-		{
-			url: tti + encodeURIComponent(text.line3),
-			height: 27,
-			fit: 'contain',
-			left: 15,
-			top: 90,
-		},
-		{
-			url: tti + encodeURIComponent(text.line4),
-			height: 27,
-			fit: 'contain',
-			left: 15,
-			top: 125,
-		},
-		{
-			url: tti + encodeURIComponent(text.line5),
-			height: 27,
-			fit: 'contain',
-			left: 15,
-			top: 160,
-		},
-		{
-			url: tti + encodeURIComponent(text.line6),
-			height: 27,
-			fit: 'contain',
-			left: 15,
-			top: 185,
-		},
-	];
-
-	const finalResponse = await fetch(imageURL, {
-		cf: {
-			image: {
-				width: 500,
-				height: 210,
-				draw: draw,
-			},
-		},
-	});
-
-	if (finalResponse.ok || finalResponse.redirected) {
-		return finalResponse
-	} else {
-		// Change to a URL on your server
-		return fetch("https://filesamples.com/samples/image/jpg/sample_640%C3%97426.jpg")
-	}
+	return finalData;
 }
