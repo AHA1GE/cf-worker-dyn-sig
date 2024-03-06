88c613c6e292a128b0358bb859793e8f37d30e0a
Harry Sun <syfsyf@me.com>
2024-03-06 17:14:33 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -179,7 +179,7 @@ async function getUserData(ip: string, token: string, request: Request): Promise
 	const ua: string = request.headers.get("user-agent") || '';
 	const parser = new UAParser(ua);
 	const result = parser.getResult();
-	console.log(result);
+	// console.log(result);
 	const uaData = {
 		os: result.os.name + " " + result.os.version,
 		browser: result.browser.name + " " + result.browser.version?.split('.')[0]
